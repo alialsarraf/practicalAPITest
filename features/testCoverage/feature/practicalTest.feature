88106d6b6eb8639cf281ce4b37aa6cda08cd0e13
Ali Al-Sarraf <ali.alsarraf@gmail.com>
2020-04-10 14:10:04 +1200

    Update test scenario

--- features/testCoverage/feature/practicalTest.feature
+++ features/testCoverage/feature/practicalTest.feature
@@ -7,9 +7,9 @@ Feature: This feature file will cover the test scenario for the API call
 @smoke
 Scenario: Assert the Acceptanc Criteria are correct when perform a GET call
  Given a user perform a GET call
-  And the response return a statusCode of 200
-  Then the total number of item return is equal to 1  
-  And each item has a fields object with a defined title
+  And returns the total number of 76 named brands of used cars
+  Then the total number of 'Kia' found and return is 1  
+  And the search did not return any usef car with the name 'Hispano Suize'
 
 
 
--- features/testCoverage/step_defintions/practical_test_steps.js
+++ features/testCoverage/step_defintions/practical_test_steps.js
@@ -8,24 +8,26 @@ Given(/^a user perform a GET call$/, async () => {
   this.response = await flux.getData();
 });
 
-When(/^the response return a statusCode of 200$/, () => {
+When('returns the total number of {int} named brands of used cars', (number) => {
   this.category = this.response.Category.Subcategories.Category;
-  expect(this.category.length).to.equal(76);
+  expect(this.category.length).to.equal(number);
+  console.log('Return number of Named brands are ', this.category.length);
 });
 
-Then(/^the total number of item return is equal to (\d+)$/, (size) => {
-  let j = 0;
+Then('the total number of {string} found and return is {int}', (carBrand, size) => {
+  let totalNumberOfKia = 0;
   for (let i = 0; i < this.category.length; i += 1) {
-    if (this.category[i].Name === 'Kia') {
-      j += 1;
+    if (this.category[i].Name === carBrand) {
+      totalNumberOfKia += 1;
     }
   }
 
-  expect(j).to.equal(size);
+  expect(totalNumberOfKia).to.equal(size);
+  console.log('Return number of Kia ', totalNumberOfKia);
 });
 
-Then(/^each item has a fields object with a defined title$/, () => {
+Then('the search did not return any usef car with the name {string}', (carName) => {
   for (let i = 0; i < this.category.length; i += 1) {
-    expect(this.category[i].Name).to.not.equal('Hispano Suize');
+    expect(this.category[i].Name).to.not.equal(carName);
   }
 });
